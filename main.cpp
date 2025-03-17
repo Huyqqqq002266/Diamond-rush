3625646c68abf93d8ec1899511ab71c9e0fa99a0
Huyqqqq002266 <duongquochuy260206@gmail.com>
2025-03-17 18:38:42 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+﻿#include <SDL.h>
+#include "TileMap.h"
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* argv[]) {
+    // Khởi tạo SDL
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Diamond Rush - Map", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    // Load bản đồ từ file level1.tmx
+    TileMap map;
+    if (!map.LoadFromFile("level1.tmx", renderer)) {
+        SDL_Log("Khong the load file level1.tmx");
+        return -1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) running = false;
+        }
+
+        SDL_RenderClear(renderer);
+        map.Render(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
