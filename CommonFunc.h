a9550a0fd15d5151bab6a33b32d7bfd7b483cb58
Huyqqqq002266 <duongquochuy260206@gmail.com>
2025-03-26 07:17:11 +0700

    Update CommonFunc.h

--- CommonFunc.h
+++ CommonFunc.h
@@ -11,8 +11,8 @@ static SDL_Window* g_window = NULL;
 static SDL_Renderer* g_screen = NULL;
 static SDL_Event g_event;
 
-const int SCREEN_WIDTH = 1280;
-const int SCREEN_HEIGHT = 640;
+const int SCREEN_WIDTH = 832;
+const int SCREEN_HEIGHT = 672;
 const int SCREEN_BPP = 32;
 
 const int COLOR_KEY_R = 167;
