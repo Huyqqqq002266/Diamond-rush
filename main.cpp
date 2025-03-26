5dc875ab8e917a4d4a2d8aa47fb6c75735c54203
Huyqqqq002266 <duongquochuy260206@gmail.com>
2025-03-26 07:26:26 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,86 +1,102 @@
-#include "tinyxml2.h"
 #include "CommonFunc.h"
 #include "BaseObject.h"
+#include "Map.h"
+#include "Enemy.h"
+#include <vector>
 
+int enemyMap[MAP_HEIGHT][MAP_WIDTH] = {0};
 BaseObject g_background;
+std::vector<Enemy> enemies;
 
 bool InitData() {
-    bool success = true;
-    int ret = SDL_Init(SDL_INIT_VIDEO);
-    if (ret != 0) {
-        return false;
-    }
-
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) return false;
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
 
     g_window = SDL_CreateWindow("Diamond Rush",
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
         SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (g_window == NULL) return false;
 
-    if (g_window == NULL) {
-        success = false;
-    }
-    else {
-        g_screen = SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED);
-        if (g_screen == NULL) {
-            success = false;
-        }
-        else {
-            SDL_SetRenderDrawColor(g_screen, 255, 255, 255, 255); // Màu trắng
-            int imgFlags = IMG_INIT_PNG;
-            if (!(IMG_Init(imgFlags) && imgFlags)) {
-                success = false;
-            }
-        }
+    g_screen = SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED);
+    if (g_screen == NULL) return false;
 
-    }
+    SDL_SetRenderDrawColor(g_screen, 255, 255, 255, 255);
+    if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG) return false;
 
-    return success;
+    return true;
 }
 
 bool LoadBackground() {
-    bool ret = g_background.LoadImg("image/background.png", g_screen);
-    if (ret = false) {
+    if (!g_background.LoadImg("image/background.png", g_screen)) {
         printf("Failed to load background image!\n");
         return false;
     }
     return true;
 }
 
-void close() {
-    g_background.Free();
-    SDL_DestroyRenderer(g_screen);
-    g_screen = NULL;
-    SDL_DestroyWindow(g_window);
-    g_window = NULL;
-    IMG_Quit();
-    SDL_Quit();
-}
-int main(int argc, char* argv[]) {
-    if (!InitData()) {
-        return -1;
-    }
-    if (LoadBackground() == false) {
-        return -1;
-    }
+void LoadEnemies() {
+    enemies.clear();
+    memset(enemyMap, 0, sizeof(enemyMap));
 
-    bool is_quit = false;
-    while (!is_quit) {
-        while (SDL_PollEvent(&g_event) != 0) {
-            if (g_event.type == SDL_QUIT) {
-                is_quit = true;
+    for (int y = 0; y < MAP_HEIGHT; y++) {
+        for (int x = 0; x < MAP_WIDTH; x++) {
+            if (tileMap[y][x] == 5) {
+                Direction dir;
+
+                if ((y == 4 && x == 5)) dir = HORIZONTAL;
+                else if ((y == 4 && x == 2) || (y == 17 && (x == 15 || x == 17 || x == 19)))
+                    dir = VERTICAL;
+                else
+                    continue;
+
+                enemies.emplace_back(x, y, dir, g_screen);
+                enemyMap[y][x] = 1;
             }
         }
+    }
+}
 
-        SDL_SetRenderDrawColor(g_screen, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR);
+
+void close(Map& gameMap) {
+    g_background.Free();
+    gameMap.Free();
+    SDL_DestroyRenderer(g_screen);
+    SDL_DestroyWindow(g_window);
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+    if (!InitData()) return -1;
+    if (!LoadBackground()) return -1;
+
+    Map gameMap;
+    if (!gameMap.LoadTiles(g_screen)) return -1;
+
+    LoadEnemies();
+
+    bool is_quit = false;
+    SDL_Event g_event;
+
+    while (!is_quit) {
+        while (SDL_PollEvent(&g_event) != 0) {
+            if (g_event.type == SDL_QUIT) is_quit = true;
+        }
+
+        SDL_SetRenderDrawColor(g_screen, 255, 255, 255, 255);
         SDL_RenderClear(g_screen);
 
-        g_background.Render(g_screen, NULL); // Vẽ background
+        g_background.Render(g_screen, NULL);
+        gameMap.DrawMap(g_screen);
 
-        SDL_RenderPresent(g_screen); // Cập nhật màn hình
+        for (Enemy& enemy : enemies) {
+            enemy.Update();
+            enemy.Render(g_screen);
+        }
+
+        SDL_RenderPresent(g_screen);
+        SDL_Delay(250);
     }
 
-    close();
-   
+    close(gameMap);
     return 0;
 }
